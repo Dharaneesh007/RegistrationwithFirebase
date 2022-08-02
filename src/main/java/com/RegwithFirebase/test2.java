9a41467fea6820636a0c4a8ccaaf81eda79a10e5
Dharaneesh007 <gsdharaneesh@gmail.com>
2022-08-02 15:25:36 +0530

    final delete

--- src/main/java/com/RegwithFirebase/test2.java
+++
@@ -1,9 +0,0 @@
-package com.RegwithFirebase;
-
-public class test2 {
-    public static void main(String[] args) {
-        System.out.println("HI Hello");
-        System.out.println("Dharneesh");
-        System.out.println("HI Hello");
-    }
-}
