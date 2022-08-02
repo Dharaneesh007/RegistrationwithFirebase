68174355489fc6fb723f23df20a6a27977732f94
Dharaneesh <66931227+Dharaneesh007@users.noreply.github.com>
2022-08-02 14:52:12 +0530

    Update test2.java

--- src/main/java/com/RegwithFirebase/test2.java
+++ src/main/java/com/RegwithFirebase/test2.java
@@ -3,5 +3,6 @@ package com.RegwithFirebase;
 public class test2 {
     public static void main(String[] args) {
         System.out.println("HI Hello");
+        System.out.println("HI Hello");
     }
 }
