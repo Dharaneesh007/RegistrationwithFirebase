d0f67013882296bd90ec76296f7b0f72fb585713
Dharaneesh007 <gsdharaneesh@gmail.com>
2022-08-03 14:10:56 +0530

    mr5

--- src/main/java/com/RegwithFirebase/Test.java
+++ src/main/java/com/RegwithFirebase/Test.java
@@ -4,7 +4,7 @@ import static java.lang.System.*;
 
 public class Test {
     public static void main(String[] args) {
-        out.println(" Hi Dharaneesh");
-
+        out.println("Hi Dharaneesh");
+        out.println("How are you");
     }
 }
